cbf1e239b667b0875cd29606d1f2fa4d2b9c4b56
SerdarAkbulut <AkbulutSerdar@outlook.com.tr>
2025-11-24 00:27:56 +0300

    added remove and delete
    element taşıma ve silme işlemleri yapıldı

--- app/components/canvas.tsx
+++ app/components/canvas.tsx
@@ -2,18 +2,22 @@
 
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../store";
-import { addElement } from "../store/builder/builderSlice";
+import {
+  addElement,
+  updateElementPosition,
+  deleteElement,
+} from "../store/builder/builderSlice";
 import ElementRenderer from "./elementRenderer";
 
 export default function Canvas() {
   const dispatch = useDispatch();
   const { grid } = useSelector((state: RootState) => state.builder.canvas);
   const elements = useSelector((state: RootState) => state.builder.elements);
-
+  const canvas = useSelector((state: RootState) => state.builder.canvas);
   const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
     e.preventDefault();
-
     const type = e.dataTransfer.getData("element-type");
+
     if (!type) return;
 
     const rect = e.currentTarget.getBoundingClientRect();
@@ -26,33 +30,91 @@ export default function Canvas() {
       y = Math.round(y / grid.size) * grid.size;
     }
 
-    const defaults: any = {
-      header: { width: 1200, height: 80 },
-      footer: { width: 1200, height: 60 },
-      card: { width: 300, height: 200 },
-      slider: { width: 1200, height: 400 },
-      "text-content": { width: 300, height: 100 },
-    };
-
-    const size = defaults[type] ?? { width: 200, height: 120 };
-
     dispatch(
       addElement({
         id: crypto.randomUUID(),
         type,
-        x,
-        y,
-        width: size.width,
-        height: size.height,
-        zIndex: elements.length + 1,
+        x: type === "header" ? "0" : type === "footer" ? "0" : x,
+        y: type === "header" ? 0 : type === "footer" ? canvas.height - 60 : y,
+        width: type === "header" || type === "footer" ? "100%" : 200,
+        height: type === "header" ? 80 : type === "footer" ? 60 : 120,
+        zIndex:
+          type === "header"
+            ? 1000
+            : type === "footer"
+            ? 0
+            : elements.length + 1,
         content: {},
       })
     );
   };
+  const handleDrag = (e: React.MouseEvent, el: any) => {
+    e.stopPropagation();
 
+    if (!el) return;
+
+    const offsetX = e.clientX - el.position.x;
+    const offsetY = e.clientY - el.position.y;
+
+    const move = (moveEvent: MouseEvent) => {
+      let newX = moveEvent.clientX - offsetX;
+      let newY = moveEvent.clientY - offsetY;
+
+      const maxX = canvas.width - el.position.width;
+      const maxY = canvas.height - el.position.height;
+
+      newX = Math.max(0, Math.min(newX, maxX));
+      newY = Math.max(0, Math.min(newY, maxY));
+
+      const isColliding = elements.some((other) => {
+        if (other.id === el.id) return false;
+
+        const A = {
+          x: newX,
+          y: newY,
+          w: el.position.width,
+          h: el.position.height,
+        };
+        const B = {
+          x: other.position.x,
+          y: other.position.y,
+          w: other.position.width,
+          h: other.position.height,
+        };
+
+        return !(
+          A.x + A.w < B.x ||
+          A.x > B.x + B.w ||
+          A.y + A.h < B.y ||
+          A.y > B.y + B.h
+        );
+      });
+
+      if (isColliding) return;
+
+      dispatch(
+        updateElementPosition({
+          id: el.id,
+          x: newX,
+          y: newY,
+        })
+      );
+    };
+
+    const stop = () => {
+      window.removeEventListener("mousemove", move);
+      window.removeEventListener("mouseup", stop);
+    };
+
+    window.addEventListener("mousemove", move);
+    window.addEventListener("mouseup", stop);
+  };
+  const handleDelete = (id: string) => {
+    dispatch(deleteElement(id));
+  };
   return (
     <div
-      className="flex bg-gray-400 relative overflow-hidden w-full h-full "
+      className={`flex bg-gray-400 relative  w-[${canvas.width}px] h-[${canvas.height}px] overflow-hidden`}
       onDragOver={(e) => e.preventDefault()}
       onDrop={handleDrop}
     >
@@ -68,6 +130,22 @@ export default function Canvas() {
             zIndex: el.position.zIndex,
           }}
         >
+          {el.type !== "header" && el.type !== "footer" && (
+            <div className="flex gap-4 justify-end">
+              <span
+                onMouseDown={(e) => handleDrag(e, el)}
+                className=" hover:cursor-pointer"
+              >
+                Taşı
+              </span>
+              <span
+                onClick={() => handleDelete(el.id)}
+                className=" hover:cursor-pointer"
+              >
+                Sil
+              </span>
+            </div>
+          )}
           <ElementRenderer element={el} />
         </div>
       ))}
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-import Image from "next/image";
 import Sidebar from "./components/sidebar";
 import Canvas from "./components/canvas";
 
 export default function Home() {
   return (
-    <div className="flex h-full  bg-zinc-50 font-sans dark:bg-black ">
-      <div className="w-full h-[800px] m-8 border border-gray-300 shadow-lg ">
+    <div className="flex h-full  bg-zinc-50 font-sans dark:bg-black gap-5 w-full">
+      <div className=" ">
         <Canvas />
       </div>
       <div className="flex justify-end ">
